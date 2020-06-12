fccb5c5b8930c1efd27bb3ebc040f0bbfafe00f7
CristianUser <cristianmejia97@gmail.com>
2020-06-11 20:15:03 -0400

    fix logger

--- app/entities/user/model.js
+++ app/entities/user/model.js
@@ -1,19 +1,15 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = sequelize => {
-  return sequelize.define(
-    'User',
-    {
-      // Model attributes are defined here
-      firstName: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      lastName: {
-        type: DataTypes.STRING
-        // allowNull defaults to true
-      }
+  return sequelize.define('User', {
+    // Model attributes are defined here
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
     },
-    { freezeTableName: true }
-  );
+    lastName: {
+      type: DataTypes.STRING
+      // allowNull defaults to true
+    }
+  });
 };
--- app/services/log.js
+++ app/services/log.js
@@ -1,7 +1,14 @@
+const path = require('path');
 const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, prettyPrint, colorize } = format;
 
 module.exports = context => {
+  if (context.file) {
+    const filePath = path.relative(process.cwd(), context.file);
+
+    context.file = filePath;
+  }
+
   return createLogger({
     defaultMeta: context,
     format: combine(timestamp(), prettyPrint(), colorize()),
